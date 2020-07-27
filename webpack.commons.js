828dd21ed20c38d149e00ac4afe173cff231d558
Huy TA QUOC <htquoc@linagora.com>
2020-07-27 17:05:16 +0700

    https://github.com/OpenPaaS-Suite/esn-frontend-inbox/issues/34: Added the usage of dotenv to load environment variables from the .env file

--- webpack.commons.js
+++ webpack.commons.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 const commonLibsPath = path.resolve(__dirname, 'node_modules', 'esn-frontend-common-libs');
 const angularCommon = path.resolve(__dirname, 'node_modules', 'esn-frontend-common-libs', 'src', 'angular-common.js');
@@ -29,6 +30,7 @@ module.exports = {
     },
   },
   plugins: [
+    new Dotenv({ systemvars: true }),
     new webpack.IgnorePlugin({ resourceRegExp: /codemirror/ }), // for summernote
     new webpack.IgnorePlugin({ resourceRegExp: /^\.\/locale$/, contextRegExp: /moment$/ }),
     new webpack.ProvidePlugin({
