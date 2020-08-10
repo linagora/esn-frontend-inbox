4f0a28c5fb7759e3b036c80a02f44095a4f40984
Tuan Tuan LE <tlcong@linagora.com>
2020-08-10 10:51:05 +0700

    #22 register i18n translation tables for linagora.esn.unifiedinbox module

--- src/linagora.esn.unifiedinbox/app/config.js
+++ src/linagora.esn.unifiedinbox/app/config.js
@@ -2,7 +2,8 @@
 
 angular.module('linagora.esn.unifiedinbox')
   .config(injectAttachmentsActionList)
-  .config(setDefaultIcon);
+  .config(setDefaultIcon)
+  .config(registerI18N);
 
 function injectAttachmentsActionList(dynamicDirectiveServiceProvider) {
   const attachmentDownloadAction = new dynamicDirectiveServiceProvider.DynamicDirective(true, 'attachment-download-action');
@@ -12,4 +13,12 @@ function injectAttachmentsActionList(dynamicDirectiveServiceProvider) {
 
 function setDefaultIcon($mdIconProvider) {
   $mdIconProvider.defaultIconSet('images/mdi/mdi.svg', 24);
+}
+
+function registerI18N($translateProvider) {
+  $translateProvider.translations('en', require('../i18n/en.json'));
+  $translateProvider.translations('fr', require('../i18n/fr.json'));
+  $translateProvider.translations('ru', require('../i18n/ru.json'));
+  $translateProvider.translations('vi', require('../i18n/vi.json'));
+  $translateProvider.translations('zh', require('../i18n/zh.json'));
 }
