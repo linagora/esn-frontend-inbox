3662a2c46f6cbc2528e77ad11739d4458fc93abd
Huy TA QUOC <htquoc@linagora.com>
2020-08-17 13:44:10 +0700

    https://github.com/OpenPaaS-Suite/esn-frontend-inbox/issues/60: Removed the usage of the 'attachments-action-list' dynamic directive

--- src/linagora.esn.unifiedinbox/app/config.js
+++ src/linagora.esn.unifiedinbox/app/config.js
@@ -1,15 +1,8 @@
 'use strict';
 
 angular.module('linagora.esn.unifiedinbox')
-  .config(injectAttachmentsActionList)
   .config(setDefaultIcon)
   .config(registerI18N);
-
-function injectAttachmentsActionList(dynamicDirectiveServiceProvider) {
-  const attachmentDownloadAction = new dynamicDirectiveServiceProvider.DynamicDirective(true, 'attachment-download-action');
-
-  dynamicDirectiveServiceProvider.addInjection('attachments-action-list', attachmentDownloadAction);
-}
 
 function setDefaultIcon($mdIconProvider) {
   $mdIconProvider.defaultIconSet('images/mdi/mdi.svg', 24);
