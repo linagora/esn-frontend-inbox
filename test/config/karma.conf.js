18598f66c9e285e7ad7ef98a2dac1cc37c7f1e17
Huy TA QUOC <htquoc@linagora.com>
2020-08-28 09:25:50 +0700

    https://github.com/OpenPaaS-Suite/esn-frontend-inbox/issues/61: Extended the timeout for tests

--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -6,6 +6,14 @@ module.exports = function(config) {
   const singleRun = process.env.SINGLE_RUN !== 'false';
 
   config.set({
+    client: {
+      mocha: {
+        // We need a timeout of at least 5000ms or else the tests will sometimes randomly
+        // fail because they exceed the default 2000ms timeout. This will happen often in
+        // the CI where the tests run slower than in our locals.
+        timeout: 5000
+      }
+    },
     basePath: '../../',
     files: [
       'src/index.test.js'
@@ -37,8 +45,6 @@ module.exports = function(config) {
       'karma-webpack',
       'karma-spec-reporter',
       'karma-sinon-chai'
-    ],
-
-    coverageReporter: { type: 'text', dir: '/tmp' },
+    ]
   });
 };
