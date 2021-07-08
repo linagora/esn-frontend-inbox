d59b758d97688b8a0f78801d1cb4e1a8d4f46892
Rachid OUBRAIM <roubraim@linagora.com>
2021-07-08 18:18:35 +0200

    customers#45 Add splitChunks in prod mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -47,12 +47,25 @@ module.exports = merge(commons, {
     new webpack.BannerPlugin(`${process.env.npm_package_name} v${process.env.npm_package_version}`)
   ],
   optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            return `${packageName.replace('@', '')}`;
+          }
+        }
+      }
+    },
     minimize: true,
     minimizer: [
-      new TerserPlugin({
-        cache: true,
-        parallel: true
-      })
+      new TerserPlugin({})
     ]
   }
 });
